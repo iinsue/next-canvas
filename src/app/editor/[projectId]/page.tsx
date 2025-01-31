5df361e9a5b0ef129af5ca62cbceb48ff13f5797
iinsue <hinsu93@gmail.com>
2025-01-31 22:05:50 +0900

    Fabric.js 초기 세팅

---
+++ src/app/editor/[projectId]/page.tsx
@@ -0,0 +1,11 @@
+import { Editor } from "@/features/editor/components/editor";
+
+const EditorProjectIdPage = () => {
+  return (
+    <>
+      <Editor />
+    </>
+  );
+};
+
+export default EditorProjectIdPage;
