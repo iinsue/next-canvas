a06d7953cd431d292ba7c9468ea659c65ba05987
iinsue <hinsu93@gmail.com>
2025-02-04 15:50:38 +0900

    에디터 Shape 겹쳤을 때 Layer 설정 기능

--- src/features/editor/components/toolbar.tsx
+++ src/features/editor/components/toolbar.tsx
@@ -4,7 +4,9 @@ import { Hint } from "@/components/hint";
 import { Button } from "@/components/ui/button";
 
 import { ActiveTool, Editor } from "@/features/editor/types";
+
 import { BsBorderWidth as BsBorderWidthIcon } from "react-icons/bs";
+import { ArrowDownIcon, ArrowUpIcon } from "lucide-react";
 
 interface ToolbarProps {
   editor: Editor | undefined;
@@ -72,6 +74,32 @@ export const Toolbar = ({
             )}
           >
             <BsBorderWidthIcon />
+          </Button>
+        </Hint>
+      </div>
+
+      <div className="flex h-full items-center justify-center">
+        <Hint label="Bring forward" side="bottom" sideOffset={5}>
+          <Button
+            variant="ghost"
+            size="icon"
+            onClick={() => editor?.bringForward()}
+            className="[&_svg]:size-4"
+          >
+            <ArrowUpIcon />
+          </Button>
+        </Hint>
+      </div>
+
+      <div className="flex h-full items-center justify-center">
+        <Hint label="Send backwards" side="bottom" sideOffset={5}>
+          <Button
+            variant="ghost"
+            size="icon"
+            onClick={() => editor?.sendBackwards()}
+            className="[&_svg]:size-4"
+          >
+            <ArrowDownIcon />
           </Button>
         </Hint>
       </div>
