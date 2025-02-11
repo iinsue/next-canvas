3fcf37d9e9d52bf1e4a1404e961fb53925c068bc
iinsue <hinsu93@gmail.com>
2025-02-12 06:02:45 +0900

    Neon & DrizzleORM & Auth.js 연동 설정

--- src/auth.ts
+++ src/auth.ts
@@ -1,6 +1,11 @@
 import NextAuth from "next-auth";
 import Github from "next-auth/providers/github";
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
 
+import { db } from "@/db/drizzle";
+
+// usersTable, accountsTable, sessionsTable, verificationTokenTable 따로 지정하지 않고 기본값으로 적용
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: DrizzleAdapter(db),
   providers: [Github],
 });
