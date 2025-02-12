ed13486cbeffd1e9e210641378405fcfeabb3f7a
iinsue <hinsu93@gmail.com>
2025-02-12 12:04:56 +0900

    구글 로그인 기능 구현

--- src/auth.ts
+++ src/auth.ts
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth";
 import Github from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
+
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
 
 import { db } from "@/db/drizzle";
@@ -7,7 +9,7 @@ import { db } from "@/db/drizzle";
 // usersTable, accountsTable, sessionsTable, verificationTokenTable 따로 지정하지 않고 기본값으로 적용
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: DrizzleAdapter(db),
-  providers: [Github],
+  providers: [Github, Google],
   pages: {
     signIn: "/sign-in",
     error: "/sign-in",
